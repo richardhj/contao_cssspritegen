80e76a7c4ca82b6368e5cf30fd9f8b578a8a32b9
richardhj <richard.henkenjohann@webwaerts.com>
2012-10-25 19:35:02 +0300

    Update config/autoload.php

--- config/autoload.php
+++ config/autoload.php
@@ -5,7 +5,7 @@
  * 
  * Copyright (C) 2005-2012 Leo Feyer
  * 
- * @package Cssspritegen
+ * @package CssSpriteGenerator
  * @link    http://contao.org
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
