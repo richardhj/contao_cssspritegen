989cc9d76c3c4ca67529f6583d3255000055d493
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2013-07-25 10:31:20 +0200

    Improve translation

--- languages/de/tl_theme.php
+++ languages/de/tl_theme.php
@@ -11,12 +11,6 @@
  * @copyright Richard Henkenjohann 2012 
  */
 
-
-/**
- * Miscellaneous
- */
-$GLOBALS['TL_LANG']['MSC'][''] = '';
-
 /**
  * Legends
  */
@@ -25,10 +19,16 @@ $GLOBALS['TL_LANG']['tl_theme']['spritegen_legend']	= 'Bilder in CSS-Sprite zusa
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_enable']			= array('Den CSS-Sprite-Generator aktivieren', 'Wenn Sie den Sprite-Generator aktivieren, haben Sie die Möglichkeit, einen Ordner mit Dateien auszuwählen, die zu einem CSS-Sprite zusammengefasst werden sollen.');
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_source']			= array('Ordner mit einzelnen Bildern auswählen', 'Wählen Sie den Ordner aus, in dem sich die Bilder befinden, die zusammengefasst werden sollen.');
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_folder']	= array('Pfad für Ausgabe des Bildes', 'Geben Sie an, wo das zusammengefasste Bild gespeichert werden soll.');
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_file']	= array('Dateiname für das Bild', 'Benennen Sie das zusammengefasste Bild. <strong>Bsp.: sprite.png</strong>');
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_direction']		= array('Anordnung der Bilder', 'Legen Sie fest, ob die Bilder nebeneinander oder untereinander platziert werden sollen.');
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_width']	= array('Breite im CSS ausgeben', 'Bestimmen Sie, ob im generierten CSS die Breite der einzelnen Bilder ausgegeben werden sollen.');
-//$GLOBALS['TL_LANG']['tl_theme']['spritegen_direction']		= array('0'=>'asdf');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_enable'] = array('Den CSS-Sprite-Generator aktivieren', 'Wenn Sie den Sprite-Generator aktivieren, haben Sie die Möglichkeit, einen Ordner mit Dateien auszuwählen, die zu einem CSS-Sprite zusammengefasst werden sollen.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_source'] = array('Ordner mit einzelnen Bildern auswählen', 'Wählen Sie den Ordner aus, in dem sich die Bilder befinden, die zusammengefasst werden sollen.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_folder'] = array('Pfad für Ausgabe des Bildes', 'Geben Sie an, wo das zusammengefasste Bild gespeichert werden soll.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_file'] = array('Dateiname für das Bild', 'Benennen Sie das zusammengefasste Bild. <strong>Bsp.: sprite.png</strong>');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_direction'] = array('Anordnung der Bilder', 'Legen Sie fest, ob die Bilder nebeneinander oder untereinander platziert werden sollen.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_width'] = array('Breite im CSS ausgeben', 'Bestimmen Sie, ob im generierten CSS die Breite der einzelnen Bilder ausgegeben werden sollen.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_modify_selectors'] = array('CSS-Selektoren selbst bestimmen', 'Legen Sie die CSS-Selektoren bei Bedarf selbst fest, damit Sie das Stylsesheet direkt ins Layout einbinden können.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_selectors'] = array('CSS-Selektoren', 'Hier legen Sie die CSS-Selektoren nach Bedarf fest. Ein "Reset" ist durch das Leeren des ersten Feldes möglich.');
+// Options
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_direction']['options'] = array(0 => 'vertikal', 1 => 'horizontal');
+// MultiColumnWizard
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_mcw_image'] = 'Bild';
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_mcw_selector'] = 'Der CSS-Selektor';
--- languages/en/tl_theme.php
+++ languages/en/tl_theme.php
@@ -1,33 +1,34 @@
-<?php 
+<?php
 
 /**
  * Contao Open Source CMS
- * 
+ *
  * Copyright (C) 2005-2012 Leo Feyer
- * 
- * @package   CssSpriteGenerator 
- * @author    Richard Henkenjohann 
- * @license   LGPL 
- * @copyright Richard Henkenjohann 2012 
+ *
+ * @package   CssSpriteGenerator
+ * @author    Richard Henkenjohann
+ * @license   LGPL
+ * @copyright Richard Henkenjohann 2012
  */
-
-
-/**
- * Miscellaneous
- */
-$GLOBALS['TL_LANG']['MSC'][''] = '';
 
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_legend']	= 'Combine images (css sprite)';
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_legend']	= 'Bilder in CSS-Sprite zusammenfassen';
 
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_enable'][0]			= 'Enable CSS sprite generator';
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_source'][0]			= 'Folder with images';
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_folder'][0]	= 'Output folder';
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_file'][0]		= 'File name';
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_direction'][0]		= 'Image direction';
-$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_width'][0]	= 'Display image size in style sheet';
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_enable'] = array('Enable the css sprite generator', 'The css sprite generator allows you to choose one folder with images that will combined.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_source'] = array('Source Folder with images', 'Choose the folder with the source images as its content.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_folder'] = array('Output path', 'Select the folder for the generated style sheet and image.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_file'] = array('Filename', 'Name the combined image. <strong>e.g.: sprite.png</strong>');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_direction'] = array('Image direction', 'Define whether the images should be placed among each other or side by side.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_output_width'] = array('Output width in CSS', 'Define whether the image size of each image should be written.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_modify_selectors'] = array('Define css selectors by own', 'Check if you want to define the css selectors by your own. So you can include the stylesheet in the layouts directly.');
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_selectors'] = array('CSS selectors', 'Define the css selectors as requested. "Reset" all fields by clearing the first row.');
+// Options
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_direction']['options'] = array(0 => 'vertical', 1 => 'horizontal');
+// MultiColumnWizard
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_mcw_image'] = 'Image';
+$GLOBALS['TL_LANG']['tl_theme']['spritegen_mcw_selector'] = 'The CSS selector';
